ff8f7cfb6a6768be0dde950990cd1cd03a398fec
Norbert Ollai <norbert_ollai@epam.com>
2018-02-28 10:54:35 +0100

    v1.0.0

---
+++ src/main/java/com/lockertracker/LockertrackerApplication.java
@@ -0,0 +1,12 @@
+package com.lockertracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockertrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockertrackerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lockertracker/LockertrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lockertracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LockertrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
